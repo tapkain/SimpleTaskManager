06743547d1e02147f8344fc2179188b8add33881
Eugene Velizhenkov <tredasaibot@gmail.com>
2018-04-15 17:53:34 +0300

    Added tasks list view controller

---
+++ SimpleTaskManager/Scenes/TaskList/TaskListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TaskListViewController.swift
+//  SimpleTaskManager
+//
+//  Created by Yevhen Velizhenkov on 4/15/18.
+//  Copyright © 2018 Yevhen Velizhenkov. All rights reserved.
+//
+
+import UIKit
+
+class TaskListViewController: UITableViewController {
+  
+}
